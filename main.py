0887b5d6c561e3e196d303522c2c585183f9b788
maurice <marius.petreanu@student.unitbv.ro>
2024-03-26 18:03:45 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+
+app = FastAPI()
+
+neigh = None
+clf = None
+
+@app.on_event("startup")
+def load_train_model():
+    df = pd.read_csv("./iris_cleaned.csv")
+    global neigh
+    neigh = KNeighborsClassifier(n_neighbors=len(np.unique(df['Y'])))
+    neigh.fit(df[df.columns[:4]].values.tolist(), df['Y'])
+    global clf
+    clf = KNN()
+    clf.fit(df[df.columns[:4]].values.tolist(), df['Y'])
+    print("Training done!")
+
+    @app.get("/anomaly")
+    def anomaly(p1: float, p2: float, p3: float, p4: float):
+        pred = clf.predict([[p1,p2,p3,p4]])
+        return "{}".format(pred[0])
+
+    @app.get("/predict")
+    def anomaly(p1: float, p2:float, p3:float, p4:float):
+        pred = clf.predict([[p1,p2,p3,p4]])
+        return "{}".format(pred[0])
+    
+
+    @app.get("/")
+    def predict(p1: float, p2: float, p3: float, p4:float):
+        pred = neigh.predict([[p1,p2,p3,p4]])
+        return "{}".format(pred[0])
+
+def read_root():
+    return {"Hello":"World"}
