bb16ebe0972f37f9b30f06c9972c729948753aa2
maurice <marius.petreanu@student.unitbv.ro>
2024-04-08 17:44:01 +0300

    update-lab

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,5 @@ COPY . .
 EXPOSE 8000
 
 RUN pip install -r requirements.txt
+
+CMD python3 main.py
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sklearn.impute import KNNImputer
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 from pyod.models.knn import KNN
+import os
 
 app = FastAPI()
 
@@ -39,3 +40,9 @@ def load_train_model():
 
 def read_root():
     return {"Hello":"World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    uvicorn.run(app, host=os.environ['HOST'],
+                port=os.environ['PORT'])
